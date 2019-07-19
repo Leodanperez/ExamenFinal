0b044a37afb3fc78c658af69862657a58fc4cb8a
Leodanperez <leodanperez.1998@gmail.com>
2019-07-19 17:56:49 -0500

    first commit

---
+++ app/src/main/java/com/example/examenfinal/Ver_Detalle_Activity.java
@@ -0,0 +1,14 @@
+package com.example.examenfinal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Ver_Detalle_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ver__detalle_);
+    }
+}
